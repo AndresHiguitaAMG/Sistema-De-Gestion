2f9b21eecb9c2c5c560c74ddf9a27a57c514cefd
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-05-09 22:03:11 -0500

    correcting errors

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -7,17 +7,17 @@ async function cargarUsuarios() {
   const request = await fetch('usuarios', {
     method: 'GET',
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
     }
   });
   const usuarios = await request.json();
-  let listadoHTML = '';
+  let listHtml = "";
   for (let usuario of usuarios) {
-  let usuarioHTML = '<tr><td>'usuario.id+'</td><td>'+usuario.nombre+' '+usuario.apellido + ' </td><td>'
-              + usuario.email+'</td><td>'+telefono
-              +' </td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></i></td></tr>';
-              listadoHTML = usuarioHTML;
-  }
-  document.querySelector("#usuarios tbody").outerHTML = listadoHTML;
+  let usuarioHtml = '<tr><td>'+usuario.id+'</td><td>' + usuario.nombre + ' '+ usuario.apellido + '</td><td>'
+                  + usuario.email+'</td><td>'+usuario.telefono
+                  + '</td><td>4644</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+                  listHtml += usuarioHtml;
+                  }
+  document.querySelector('#usuarios tbody').outerHTML = listHtml;
 }
