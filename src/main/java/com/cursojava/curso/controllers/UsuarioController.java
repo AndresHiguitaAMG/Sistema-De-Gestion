8b83f7e23e0cdd2a3c6037865fee2c7ff329d0ad
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-05-24 17:18:45 -0500

    Added remove users feature

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import com.cursojava.curso.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,7 +14,7 @@ import java.util.List;
 public class UsuarioController {
     @Autowired
     private UsuarioDao usuarioDao;
-    @RequestMapping(value = "api/usuario/{id}")
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.GET)
     public Usuario getUsuario(@PathVariable long id) {
         Usuario usuario = new Usuario();
         usuario.setId(id);
@@ -28,6 +29,18 @@ public class UsuarioController {
         return usuarioDao.getUsuarios();
     }
     @RequestMapping(value = "usuario789")
+    public Usuario editar() {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andres");
+        usuario.setApellido("Higuita");
+        usuario.setEmail("andreshiguitaamg@gmail.com");
+        usuario.setTelefono("3164256707") ;
+        return usuario;
+    }
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@PathVariable Long id) {
+        usuarioDao.eliminar(id);
+    }
     public Usuario buscar() {
         Usuario usuario = new Usuario();
         usuario.setNombre("Andres");
